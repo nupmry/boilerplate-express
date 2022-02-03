a0a6f4bb2f72859c3df01bb5c44e7e44876d2c52
Anupam Arya <68789516+nupmry@users.noreply.github.com>
2022-02-03 13:05:25 +0000

    challenge 4

--- myApp.js
+++ myApp.js
@@ -2,16 +2,18 @@ var express = require('express');
 var app = express();
 
 
-console.log("Hello World")
+// console.log("Hello World")
 
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/views/index.html")
-})
+app.use('/public', express.static(__dirname + "/public"))
 
-app.get('/', (req, res) => {
-  res.send("Hello Express")
-})
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + "/views/index.html")
+// })
+
+// app.get('/', (req, res) => {
+//   res.send("Hello Express")
+// })
 
 
 
