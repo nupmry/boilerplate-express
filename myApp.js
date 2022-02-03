6b6a3c928c5fd58cc7e320d5362a31777494c5c0
Anupam Arya <68789516+nupmry@users.noreply.github.com>
2022-02-03 12:57:27 +0000

    challenge 3

--- myApp.js
+++ myApp.js
@@ -5,7 +5,9 @@ var app = express();
 console.log("Hello World")
 
 
-
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/views/index.html")
+})
 
 app.get('/', (req, res) => {
   res.send("Hello Express")
